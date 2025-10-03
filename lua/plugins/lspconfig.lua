697a58ce300585d5bde04bdf9bec791cc465c175
praveenscript <praveenkumarjkr@gmail.com>
2025-10-03 21:02:50 +0530

    just committing to get it back if I mess up lol

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -16,15 +16,56 @@ return { -- Main LSP Configuration
 	config = function()
 		-- local capabilities = require("blink.lsp").default_capabilities()
 
-		-- local function suppress_signature_help(_, _)
-		-- 	vim.lsp.handlers["textDocument/signatureHelp"] = function()
-		-- 		print("supressing signature Help")
-		-- 	end
-		-- end
+
+
+
+
+
+
+
+		local util = require 'lspconfig.util'
+
+		local root_files = {
+			'.luarc.json',
+			'.luarc.jsonc',
+			'.luacheckrc',
+			'.stylua.toml',
+			'stylua.toml',
+			'selene.toml',
+			'selene.yml',
+		}
+
+
 
 		vim.lsp.config("lua_ls", {
 
-			-- on_attach = suppress_signature_help,
+			root_dir = function(fname)
+				local bufname = vim.api.nvim_buf_get_name(0)
+				local is_luapad = bufname:match("Luapad") or bufname:match("luapad.nvim")
+
+				if is_luapad then
+					-- Use buffer's directory as root to avoid scanning project
+					return nil
+				end
+
+				local util = require("lspconfig.util")
+				return util.find_git_ancestor(fname) or util.path.dirname(fname)
+			end,
+			-- on_attach = function(client, bufnr)
+			-- 	local bufname = vim.api.nvim_buf_get_name(bufnr)
+			-- 	if bufname:match("Luapad") or bufname:match("luapad.nvim") then
+			-- 		vim.schedule(function()
+			-- 			client.stop()
+			-- 			vim.notify("Detached lua_ls from Luapad buffer", vim.log.levels.INFO)
+			-- 		end)
+			-- 		return
+			-- 	end
+			-- 	-- Normal attach logic here (if needed)
+			-- end,
+
+
+
+
 			settings = {
 				lua = {
 					runtime = {
